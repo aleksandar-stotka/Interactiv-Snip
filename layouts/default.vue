1dc00990e98709861e2d616ce391088a1d1c59b3
aleksandar-stotka <aangelevski6@gmail.com>
2023-12-12 09:53:52 -0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex justify-center items-start">
+        <div>
+        <h1>Navbar</h1>
+    </div>
+    <div>
+        <h1>sidebar</h1>
+    </div>
+</div>
+    <div>
+        <h1>Footer</h1>
+    </div>
+
+    
+    
+  
+  
+  
+  
+  <slot/>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
